bfb51bd46ecf11e06a9c4bd53d4a3877aeedbf0b
ricardosoliveira <ricardo.kamui@gmail.com>
2022-06-09 13:40:41 -0300

    subindo app node

---
+++ app_node/node/app/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+
+}
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people ( name ) VALUES ('Ricardo');`
+connection.query(sql)
+connection.end()
+
+app.get('/', (req, res) => {
+    res.send('<h1>FullCycle</h1>')
+})
+
+app.listen(port, () => {
+    console.log("Escutando na porta " + port)
+})
