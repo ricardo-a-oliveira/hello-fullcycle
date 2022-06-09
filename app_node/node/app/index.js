4b3a04ee1cbbdde47f987d2d5a75241c4fd81f33
ricardosoliveira <ricardo.kamui@gmail.com>
2022-06-09 16:08:25 -0300

    leitura do banco

--- app_node/node/app/index.js
+++ app_node/node/app/index.js
@@ -14,11 +14,20 @@ const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
 const sql = `INSERT INTO people ( name ) VALUES ('Ricardo');`
+const querySQL = `SELECT name FROM people WHERE id = 1;`
+
+let nome;
+
 connection.query(sql)
-connection.end()
+connection.query(querySQL, (err, result, fields) => {
+    console.log("result = " + JSON.stringify(result))
+    nome = result[0].name
+})
+//connection.end()
 
 app.get('/', (req, res) => {
-    res.send('<h1>FullCycle</h1>')
+    res.send('<h1 style="text-align=center">FullCycle</h1><br /><br /><br />' +
+              '<p style="text-align=center">'+ nome + '</p>')
 })
 
 app.listen(port, () => {
